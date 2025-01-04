e01595d5f6817f697161f8ba7d49bc607bf9962d
pache <ety.raquin@orange.fr>
2025-01-04 09:46:07 +0100

    update keychron c1 rgb pache : move key to enter fn layer

--- keyboards/keychron/c1/rgb/keymaps/pache/keymap.c
+++ keyboards/keychron/c1/rgb/keymaps/pache/keymap.c
@@ -143,11 +143,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {   _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______,             _______, _______, _______ },
         // |--------------------------------------------------------------------------------------------------------------------------------------------------------------------|
         // | TAB        | A      | Z      | E      | R      | T      | Y      | U      | I      | O      | P      | ^¨     | $£     | *µ          |  | DEL    | END    | PGDN   |
-        {   _______,     KC_NO,   KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_PGUP, _______, _______,         _______, _______, _______ },
+        {   _______,     KC_NO,   KC_LSFT, KC_LSFT, KC_INS,  KC_NO,   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_PGUP, _______, _______,         _______, _______, _______ },
         // |--------------------------------------------------------------------------------------------------------------------------------------------------------------------|
         // | CAPSLCK      | Q      | S      | D      | F      | G      | H      | J      | K      | L      | M      | ù%     | RETURN             |
-        {   _______,       KC_NO,   KC_NO,   KC_LSFT, KC_INS,  MO(L_FNCT),
-                                                                        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_PGDN, K_RET(_______),         _______, _______, _______ },
+        {   _______,       KC_NO,   KC_NO,   KC_LSFT, MO(L_FNCT),
+                                                               KC_NO,   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_PGDN, K_RET(_______),         _______, _______, _______ },
         // |--------------------------------------------------------------------------------------------------------------------------------------|           |--------|
         // | LSHIFT            | W      | X      | C      | V      | B      | N      | ,?     | ;.     | :/     | !§     | RSHIFT                 |           | UP     |
         {   K_LS(_______),      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_ESC,  KC_BSPC, KC_ENT,  KC_DEL,  KC_NO,   K_RS(KC_CAPS),              _______, _______, _______ },
